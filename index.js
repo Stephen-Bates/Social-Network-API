3dddc17e512b20b721a5fdb4aaff4a2cd0ee9d94
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-05-01 18:29:29 -0400

    added server file

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// TODO: Might change this up a little be, make it more descriptive
+const activity = cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
